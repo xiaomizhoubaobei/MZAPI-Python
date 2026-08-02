8e49f3735f2f0c3c863ed0c2f938de4fde5b3c7c
qixiaoxin <qixiaoxin@stu.sqxy.edu.cn>
2026-08-02 19:45:01 +0800

    docs: 为阿里云凭证http选项模块补充文件头与维护注释
    为 mzapi/core/alicloudauth/credentials/http 下的 __init__.py 与
    _options.py 新增 MPL 2.0 文件头部、内部项目标识及模块/类/方法注释，
    与 tencentauth/circuit_breaker.py 保持一致的元数据规范，便于维护。
    
    关联 #59

--- mzapi/core/alicloudauth/credentials/http/__init__.py
+++ mzapi/core/alicloudauth/credentials/http/__init__.py
@@ -1,3 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2026 祁筱欣
+#
+# ORIGINAL IMPLEMENTATION - DO NOT REMOVE OR ALTER THIS NOTICE
+# This file is part of MZAPI and is licensed under MPL 2.0.
+# Any modifications to this file must remain under MPL 2.0
+# when redistributed.
+
+# 内部项目标识（请勿修改）
+_MZAPI_ORIGIN = "mzapi-alicloud-credentials-http-init-2026-qxx"
+
+"""
+阿里云凭证 HTTP 选项包
+
+聚合阿里云凭证提供方共用的 HTTP 请求选项配置，对外统一导出
+``HttpOptions``，便于各凭证提供方以一致的方式配置代理与超时。
+
+导出：
+  - HttpOptions：HTTP 请求选项配置类
+"""
+
 from ._options import HttpOptions
 
 __all__ = [
